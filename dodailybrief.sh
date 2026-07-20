18ce0205be61f98e842effd410d1f27fb6857dbb
Steve Wolfe <swolfe100@gmail.com>
2026-07-20 13:08:56 -0400

    change Linux shell programs - remove the file send, create a new file send script

--- dodailybrief.sh
+++ dodailybrief.sh
@@ -12,5 +12,3 @@ cd /home/test/project
 /home/test/.local/bin/claude --model haiku  -p "run briefing.md" --tools "Read,Write,WebFetch,WebSearch" --allowedTools "Read,Write,WebFetch,WebSearch"  > dailybrief.log 2>&1
 
 
-# This script used to assume that the AI would copy the file 
-# It would be better to copy the file with the script that drives the ai
---
+++ dosenddailybrief.sh
@@ -0,0 +1,5 @@
+set -euo pipefail
+
+#use the ftp
+ncftpput -f ncftp.conf . dailybrief.html
+
